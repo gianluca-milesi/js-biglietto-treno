206335c94008a0087fd7618e1724bf35e61cc499
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-14 15:55:04 +0200

    ADD: final price calculation according to discounts

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//Variabili
+let numKm = prompt("Quanti km hai percorso?"); //string
+let eta = prompt("Quanti anni hai?"); //string
+numKm = parseFloat(numKm); //number decimali
+eta = parseInt(eta); //number interi
+let ticketBasePrice = 0.21 * numKm; //number
+let discount = 0;
+
+
+//Sconti
+if(eta < 18) {
+    discount = ticketBasePrice * (20/100);
+} 
+if (eta > 65) {
+    discount = ticketBasePrice * (40/100);
+}
+
+
+//Output
+let ticketFinalPrice = ticketBasePrice - discount;
+console.log(`Il prezzo finale da pagare è ${ticketFinalPrice}€`);
+
+
+
+
