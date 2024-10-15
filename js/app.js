627b9ba4714f53f69d9a8289a8e836f3108ee473
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-15 17:35:47 +0200

    ADD: alert messages about invalid values ​​entered in prompts

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
 //Variabili
 let numKm = prompt("Quanti km hai percorso?"); //string
-let eta = prompt("Quanti anni hai?"); //string
 numKm = parseFloat(numKm); //number decimali
-eta = parseInt(eta); //number interi
-let ticketBasePrice = 0.21 * numKm; //number
+let ticketBasePrice = 0.21 * numKm; // number
 let discount = 0;
 
+if (isNaN(numKm) || numKm < 0) {
+    alert("Per favore, inserisci un numero valido per i km percorsi.");
+    location.reload(); // ricarica la pagina
+} else {
 
-//Sconti
-if (eta < 18) {
-    discount = ticketBasePrice * (20 / 100);
-}
-if (eta > 65) {
-    discount = ticketBasePrice * (40 / 100);
-}
+    let eta;
 
+    while (true) {
+        eta = prompt("Quanti anni hai?"); // string
+        eta = parseInt(eta);
+        if (isNaN(eta) || eta < 0 || !Number.isInteger(eta)) {
+            alert("Per favore, inserisci un numero intero e valido per l'età.");
+        } else {
+            break;
+        }
+    }
+
+    // Sconti
+    if (eta < 18) {
+        discount = ticketBasePrice * (20 / 100);
+    }
+    if (eta > 65) {
+        discount = ticketBasePrice * (40 / 100);
+    }
+
+}
 
 //Output
 let ticketFinalPrice = ticketBasePrice - discount;
@@ -24,3 +39,11 @@ const formattedPrice = new Intl.NumberFormat("it-IT", {
 }).format(ticketFinalPrice);
 // ticketFinalPrice = ticketFinalPrice.toFixed(2);
 console.log(`Il prezzo finale da pagare è ${formattedPrice}`);
+
+
+
+
+// Versione compatta
+// const priceKm = 0.21, km = parseInt(prompt("Inserisci i km")), eta = parseInt(prompt("Inserisci la tua età"));
+// const discount = (km * priceKm * (eta < 18 ? 20 : eta >= 65 ? 40 : 0)) / 100;
+// console.log((km * priceKm - discount).toFixed(2));
